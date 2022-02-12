92788de2d9746ab14aa4fa6e3a65ff655b108d84
concagabriel <58643247+concagabriel@users.noreply.github.com>
2022-02-12 14:28:20 -0300

    Update __manifest__.py

--- sat_default/__manifest__.py
+++ sat_default/__manifest__.py
@@ -61,6 +61,8 @@
         'https://github.com/OCA/manufacture.git -b 13.0',
         'https://github.com/OCA/manufacture-reporting.git -b 13.0',
         'https://github.com/OCA/helpdesk -b 13.0',
+        'https://github.com/OCA/pos.git -b 13.0',
+        'https://github.com/OCA/report-print-send.git -b 13.0',
     ],
 
     'docker-images': [
